9f78465d67d3926537b68a751c9bbae0875d9d54
Ilsiya <118172101+IlsiyaNasibullina@users.noreply.github.com>
2024-05-13 19:13:56 +0300

    Update App.scala

--- src/main/scala/com/paranid5/auth_service/App.scala
+++ src/main/scala/com/paranid5/auth_service/App.scala
@@ -16,7 +16,7 @@ import org.http4s.{Request, Response}
 
 object App extends IOApp:
   override def run(args: List[String]): IO[ExitCode] =
-    println("BIBA")
+    Exception("BIBA").printStackTrace()
     AppModule(runServer() run _) map:
       _.fold(fa = _ ⇒ ExitCode.Error, fb = _ ⇒ ExitCode.Success)
 
